f2d4eb841522c43310942992058e4a9ab75c8bd0
Nithishkumar007 <potlapula.nithish@gmail.com>
2024-06-06 10:50:58 +0530

    Committed

---
+++ if.js
@@ -0,0 +1,14 @@
+
+        var age=22;
+        var gender="Female";
+        console.log("Outout is:");
+        if(age>20 && gender=="Male"){
+           console.log("Go to 1st floor.");
+        }
+        else if(age >20 && gender=="Female"){
+           console.log("Go to 2nd floor.");
+        }
+        else if(age <20&& gender=="children"){
+           console.log("Go to 3rd floor.")
+        }
+    
